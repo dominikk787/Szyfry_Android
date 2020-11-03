d9f7a8939d1f80e41481c9bbf1bf90d0b52dae88
dominikk787 <dominikk787@gmail.com>
2020-11-03 15:19:46 +0100

    Initial commit

---
+++ app/src/main/java/com/dominikk787/szyfry/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dominikk787.szyfry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
